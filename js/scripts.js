13aedc022af7bfb0a906a4be830be0079521b24d
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-18 09:27:57 +0300

    Add hide and show jquery what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
   $(".fig1").click(function(){
     $(this).hide()
-    $(".description1").show();
+    $("#description1").show();
   });
-  $(".description1").click(function(){
+  $("#description1").click(function(){
     $(this).hide()
     $(".fig1").show();
-  })
+  });
+  $(".fig2").click(function(){
+    $(this).hide()
+    $("#description2").show();
+  });
+  $("#description2").click(function(){
+    $(this).hide()
+    $(".fig2").show();
+  });
+  $(".fig3").click(function(){
+    $(this).hide()
+    $("#description3").show();
+  });
+  $("#description3").click(function(){
+    $(this).hide()
+    $(".fig3").show();
+  });
 });
